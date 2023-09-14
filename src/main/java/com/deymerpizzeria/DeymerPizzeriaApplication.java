120c00b225428406f78b964d747aff5bf47df55a
Deymer Hernandez <deymerh@MacBook-Air-de-Deymer.local>
2023-09-13 20:15:27 -0500

    Structure initial of project

---
+++ src/main/java/com/deymerpizzeria/DeymerPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.deymerpizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeymerPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeymerPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deymerpizzeria/DeymerPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deymerpizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeymerPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
